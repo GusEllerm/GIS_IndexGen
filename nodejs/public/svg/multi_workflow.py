57795ebf6d38dc7d304addca7843c7c207861f34
Augustus Ellerm <ellermaugustus@gmail.com>
2022-03-02 10:23:38 +1300

    Changing directory structure to better delineate between the workflow (CWL) service, the web app (nodejs) service, and the database (MySQL) service.

--- nodejs/public/svg/multi_workflow.py
+++ nodejs/public/svg/multi_workflow.py
@@ -1,4 +1,4 @@
 import os
 
-CWL_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "..", "CWL"))
+CWL_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "..", "..", "CWL"))
 os.system("cwltool --print-dot " + str(CWL_folder) + "/Workflows/multi_workflow.cwl | dot -Tsvg > multi_workflow.svg")
--- nodejs/public/svg/workflow.py
+++ nodejs/public/svg/workflow.py
@@ -1,4 +1,4 @@
 import os
 
-CWL_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "..", "CWL"))
+CWL_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "..", "..", "CWL"))
 os.system("cwltool --print-dot " + str(CWL_folder) + "/Workflows/workflow.cwl | dot -Tsvg > workflow.svg")
